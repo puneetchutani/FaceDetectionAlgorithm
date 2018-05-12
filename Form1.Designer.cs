4d9bfb58e7fde1beca1acad3e62b61859a5585ef
puneetchutani <puneet@smarttrade.biz>
2018-05-12 19:49:08 +1200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace FaceDetectionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dsViewer1 = new Dynamsoft.Forms.DSViewer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start capturing image ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(386, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Recognising the emotions";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dsViewer1
+            // 
+            this.dsViewer1.Location = new System.Drawing.Point(79, 113);
+            this.dsViewer1.Name = "dsViewer1";
+            this.dsViewer1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dsViewer1.SelectionRectAspectRatio = 0D;
+            this.dsViewer1.Size = new System.Drawing.Size(301, 320);
+            this.dsViewer1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(386, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 320);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(79, 473);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 580);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dsViewer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private Dynamsoft.Forms.DSViewer dsViewer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dynamsoft.UVC;
+using Dynamsoft.Core;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Common.Contract;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Data.SqlClient;
+
+namespace FaceDetectionApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("be0e0b4c68414c3b8704092ee4fb96a2", "https://westcentralus.api.cognitive.microsoft.com/face/v1.0");
+        private CameraManager m_CameraManager = null;
+        private Camera m_CurrentCamera = null;
+        string path = string.Empty;
+        private static int counter = 0;
+        private ImageCore m_ImageCore = null;
+        public Form1()
+        {
+            InitializeComponent();
+            path = Path.Combine(Environment.CurrentDirectory, "img.jpeg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            InitialiseAndStartWebCam();
+            startTimer();
+            Cursor = Cursors.Arrow;
+        }
+
+        private void InitialiseAndStartWebCam()
+        {
+            //Initialise the webcamera and start
+            m_CameraManager = new CameraManager("t0068MgAAALy2+1uzMH3do0eStkxwYPt8igdrUGRffaUDbP8qgfsa+K8waVxspoiiFvfyFNcuWorQR1uXAoOCvPJ3eKcLELE=");
+            m_ImageCore = new ImageCore();
+            dsViewer1.Bind(m_ImageCore);
+            if (m_CameraManager.GetCameraNames() != null)
+            {
+                m_CurrentCamera = m_CameraManager.SelectCamera(0);
+                m_CurrentCamera.SetVideoContainer(pictureBox1.Handle);
+                m_CurrentCamera.Open();
+            }
+        }
+
+        private void startTimer()
+        {
+            timer1 = new Timer();
+            timer1.Start();
+            timer1.Interval = 1000;
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private async void Timer1_Tick(object sender, EventArgs e)
+        {
+            button1.Text = "Image will capture in " + (10 - counter).ToString() + " seconds";
+            counter++;
+            if (counter % 10 == 0)
+            {
+                counter = 0;
+                timer1.Stop();
+                button1.Text = "Capturing...";
+                var key = await GrabImageAndStoreEmotions();
+                button1.Text = "Captured!";
+                //label1.Text = "";
+                SaveImageAndEmotions(key);
+                startTimer();
+            }
+        }
+
+        private async Task<string> GrabImageAndStoreEmotions()
+        {
+            string key = string.Empty;
+            try
+            {
+                var currentImage = m_CurrentCamera.GrabImage();
+                Bitmap bit =new Bitmap(currentImage);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    // Convert Image to byte[]
+                    bit.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    //dpgraphic.image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                    byte[] imageBytes = ms.ToArray();
+                    var faces = await UploadAndDetectFaces(ms);
+                    if (faces.Any())
+                    {
+                        var face = faces[0];
+                        var emotionScores = face.FaceAttributes.Emotion;
+                        key = emotionScores.ToRankedList().First().Key;
+                    }
+                }
+                //currentImage.Dispose();
+                //currentImage = null;
+
+                //string outputFileName = "...";
+                //using (MemoryStream memory = new MemoryStream())
+                //{
+                //    using (FileStream fs = new FileStream(outputFileName, FileMode.Create, FileAccess.ReadWrite))
+                //    {
+                //        currentImage.Save(memory, ImageFormat.Jpeg);
+                //        byte[] bytes = memory.ToArray();
+                //        fs.Write(bytes, 0, bytes.Length);
+                //    }
+                //}
+                //bit.Save(path);
+
+                m_ImageCore.IO.LoadImage(bit);
+                label1.Text = "The emotion is: " + key;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+                return null;
+            }
+            return key;
+        }
+
+        private void SaveImageAndEmotions(string key)
+        {
+            try
+            {
+                //Read Image Bytes into a byte array
+                byte[] imageData = ReadFile(path);
+
+                //Initialize SQL Server Connection
+                SqlConnection con = new SqlConnection("Data Source=LISA;Initial Catalog=PIAEData;Integrated Security=True");
+                con.Open();
+                string qry = "insert into ImageStore (ImageStore, Emotions) values(@ImageStore, @Emotions)";
+                SqlCommand SqlCom = new SqlCommand(qry, con);
+
+                //We are passing Original Image Path and 
+                //Image byte data as SQL parameters.
+                SqlCom.Parameters.Add(new SqlParameter("@ImageStore", (object)imageData));
+                SqlCom.Parameters.Add(new SqlParameter("@Emotions", (object)key));
+
+                //Open connection and execute insert query.
+                SqlCom.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error");
+            }
+        }
+
+        byte[] ReadFile(string sPath)
+        {
+            byte[] data = null;
+            FileInfo fInfo = new FileInfo(sPath);
+            long numBytes = fInfo.Length;
+            FileStream fStream = new FileStream(sPath, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fStream);
+            data = br.ReadBytes((int)numBytes);
+            return data;
+        }
+
+        private async Task<Face[]> UploadAndDetectFaces(Stream imageFileStream)
+        {
+            IEnumerable<FaceAttributeType> faceAttributes = new FaceAttributeType[] { FaceAttributeType.Gender, FaceAttributeType.Age, FaceAttributeType.Smile, FaceAttributeType.Emotion, FaceAttributeType.Glasses };
+            // Call the Face API.
+            try
+            {
+                Face[] faces = await faceServiceClient.DetectAsync(imageFileStream, returnFaceId: true, returnFaceLandmarks: false, returnFaceAttributes: faceAttributes);
+                return faces;
+            }
+            catch (FaceAPIException f)
+            {
+                MessageBox.Show(f.ErrorMessage, f.ErrorCode);
+                return new Face[0];
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error");
+                return new Face[0];
+            }
+        }
+
+        //private async Task<Face[]> UploadAndDetectFaces(string imageFilePath)
+        //{
+        //    IEnumerable<FaceAttributeType> faceAttributes = new FaceAttributeType[] { FaceAttributeType.Gender, FaceAttributeType.Age, FaceAttributeType.Smile, FaceAttributeType.Emotion, FaceAttributeType.Glasses };
+        //    // Call the Face API.
+        //    try
+        //    {
+        //        using (Stream imageFileStream = File.OpenRead(imageFilePath))
+        //        {
+        //            Face[] faces = await faceServiceClient.DetectAsync(imageFileStream, returnFaceId: true, returnFaceLandmarks: false, returnFaceAttributes: faceAttributes);
+        //            return faces;
+        //        }
+        //    }
+        //    catch (FaceAPIException f)
+        //    {
+        //        MessageBox.Show(f.ErrorMessage, f.ErrorCode);
+        //        return new Face[0];
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        MessageBox.Show(e.Message, "Error");
+        //        return new Face[0];
+        //    }
+        //}
+
+        //private async void button3_Click(object sender, EventArgs e)
+        //{
+        //    var faces = await UploadAndDetectFaces(path);
+        //    if (faces.Any())
+        //    {
+        //        var face = faces[0];
+        //        var emotionScores = face.FaceAttributes.Emotion;
+        //        var rank = emotionScores.ToRankedList().First();
+        //        var key = rank.Key;
+        //        var value = rank.Value*100;
+        //    }
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button1.Text = "Start capturing image";
+            //TODO analyse the emotions thats is maximum from the database 
+        }
+    }
+}
